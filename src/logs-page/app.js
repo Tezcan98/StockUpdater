8ea5a817800fb452783bf3aaf77f9a92f7e7cdf8
Enes Tezcan <tezcan_enes@hotmail.com>
2022-04-21 06:13:59 +0300

    somenewpages

--- src/logs-page/app.js
+++ src/logs-page/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'decreased',
+                message: 'increased',
                 // location: ret.data.trim()
             })
         }
