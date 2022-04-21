e08ba72f96d0c91a91176c6ea9cf22a03fe11f0c
Enes Tezcan <tezcan_enes@hotmail.com>
2022-04-21 07:26:50 +0300

    databasenew

--- src/database-helper/app.js
+++ src/database-helper/app.js
@@ -14,16 +14,27 @@ let response;
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  * 
  */
+const ServerlessClient = require('serverless-postgres')
+
+const client = new ServerlessClient({
+    user: 'postgres',
+    host: 'iotdatabase.cuxjnv65wifa.us-east-2.rds.amazonaws.com',
+    database: 'iotstokdata',
+    password: '12345678',
+    port: '5432',
+    debug: true,
+    delayMs: 3000,
+});
+
 exports.lambdaHandler = async (event, context) => {
     try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'increased',
-                // location: ret.data.trim()
-            })
-        }
+		await client.connect();
+		const result = await client.query(`SELECT 1+1 AS result`);
+		await client.clean();
+		return {
+		  body: JSON.stringify({message: result.rows[0]}),
+		  statusCode: 200
+		}
     } catch (err) {
         console.log(err);
         return err;
